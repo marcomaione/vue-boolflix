106ba7c4e2e4482f37df14131ac149ab7f101225
marco maione <maionemarco14@gmail.com>
2022-02-26 12:42:35 +0100

    aggiornamento

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <MyHeader></MyHeader>
+    <MyMain></MyMain>
   </div>
 </template>
 
 <script>
 import MyHeader from './components/MyHeader.vue'
+import MyMain from './components/MyMain.vue'
 
 export default {
   name: 'App',
   components: {
     MyHeader,
+    MyMain,
   }
 }
 </script>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+name:"MyMain",
+}
+</script>
+
+<style lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.container {
+    background-color:#434343;
+    width: 100%;
+    min-height: calc(100vh - 80px);
+}
+
+</style>
