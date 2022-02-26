c68140c4d744643b89e689f9e49d57c6daed45f4
marco maione <maionemarco14@gmail.com>
2022-02-26 16:38:37 +0100

    aggiornamento

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,22 @@
 import MyHeader from './components/MyHeader.vue'
 import MyMain from './components/MyMain.vue'
 
+const axios = require('axios');
+
 export default {
   name: 'App',
   components: {
     MyHeader,
     MyMain,
+  },
+  methods: {
+    getFilm() {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=431bdbe406cda6cf376d9ec554586227&query=star Wars')
+      .then((response) => {
+        this.MyMain = response.data.response;
+
+      })
+    }
   }
 }
 </script>
