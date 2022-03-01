1276891e720d23811f1d4d9972f39a6763840530
marco maione <maionemarco14@gmail.com>
2022-03-01 21:27:22 +0100

    aggiornamento

--- src/components/partials/MyCard.vue
+++ src/components/partials/MyCard.vue
@@ -2,11 +2,15 @@
     <li class="card">
         <div>{{getTitle()}}</div>
         <div>{{info.original_title}}</div>
+        <img v-if="info.poster_path" :src="`https://image.tmdb.org/t/p/w342${info.poster_path}`">
+        <img v-else :src="'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled.png'">
         <div>
             <img v-if="languages.includes(info.original_language)" :src="require('../../assets/flags/' + info.original_language + '.png')">
             <span v-else>{{info.original_language}}</span>
         </div>
-        <div>{{info.vote_average}}</div>
+        <div>
+            <i v-for="i in 5" :key="i" class="fa-star" :class="(i<getstar())?'fa-solid' : 'fa-regular'"></i>
+        </div>
     </li>
 </template>
 
@@ -29,6 +33,9 @@ export default {
             }else {
                 return this.info.name;
             }
+        },
+        getstar() {
+            return Math.ceil(this.info.vote_average / 2);
         }
     }
 
